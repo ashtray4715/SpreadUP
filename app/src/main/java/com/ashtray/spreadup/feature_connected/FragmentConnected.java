ddb5ab2c408166e730b91bc9a037a1766d3d152e
ashtray4715 <52469753+ashtray4715@users.noreply.github.com>
2020-04-16 01:06:24 +0600

    Added bottom navigation view for showing multiple connection options

--- app/src/main/java/com/ashtray/spreadup/feature_connected/FragmentConnected.java
+++ app/src/main/java/com/ashtray/spreadup/feature_connected/FragmentConnected.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -18,6 +19,7 @@ import com.ashtray.spreadup.SpreadUpApplication;
 import com.ashtray.spreadup.entities.MyFragment;
 import com.gobinda.DTConnectedClient;
 import com.gobinda.DTManager;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class FragmentConnected extends MyFragment {
 
@@ -26,6 +28,27 @@ public class FragmentConnected extends MyFragment {
 
     public FragmentConnected() {
         // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_connected, container, false);
+
+        BottomNavigationView bottomNavigationView = v.findViewById(R.id.BottomNavigationViewForConnectedOptions);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new MyNavigationItemSelectedListener());
+
+        DTManager.getInstance().setDtManagerConnCallBack(new DTManagerConnCallBackHandler());
+        textViewForShowingConnectionStatus = v.findViewById(R.id.TextViewForShowingConnectionStatus);
+        updateConnectionStatus();
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        myFragmentCallBacks.setBackButtonEnabled(true);
+        myFragmentCallBacks.setActivityTitle(SpreadUpApplication.getInstance().getString(R.string.fragment_title_connected));
     }
 
     @Override
@@ -40,117 +63,105 @@ public class FragmentConnected extends MyFragment {
         }
     }
 
-    private void showReceivingStatusFragment() {
+    private boolean showReceivingStatusFragment() {
         if(currentShowingFragment != null && currentShowingFragment instanceof FragmentReceivingStatus) {
-            return;
+            return false;
         }
         if(getActivity() != null) {
             getActivity().runOnUiThread(()->{
                 currentShowingFragment = new FragmentReceivingStatus();
                 FragmentManager fm = getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fm.beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_container, currentShowingFragment);
+                fragmentTransaction.replace(R.id.RelativeLayoutForConnectedOptions, currentShowingFragment);
                 fragmentTransaction.show(currentShowingFragment);
                 fragmentTransaction.commit();
             });
         }
+        return true;
     }
 
-    private void showSendingStatusFragment() {
+    private boolean showSendingStatusFragment() {
         if(currentShowingFragment != null && currentShowingFragment instanceof FragmentSendingStatus) {
-            return;
+            return false;
         }
         if(getActivity() != null) {
             getActivity().runOnUiThread(()->{
                 currentShowingFragment = new FragmentSendingStatus();
                 FragmentManager fm = getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fm.beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_container, currentShowingFragment);
+                fragmentTransaction.replace(R.id.RelativeLayoutForConnectedOptions, currentShowingFragment);
                 fragmentTransaction.show(currentShowingFragment);
                 fragmentTransaction.commit();
             });
         }
+        return true;
     }
 
-    private void showSendingFileFragment() {
+    private boolean showSendDataFragment() {
         if(currentShowingFragment != null && currentShowingFragment instanceof FragmentSendData) {
-            return;
+            return false;
         }
         if(getActivity() != null) {
             getActivity().runOnUiThread(()->{
                 currentShowingFragment = new FragmentSendData();
                 FragmentManager fm = getActivity().getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fm.beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_container, currentShowingFragment);
+                fragmentTransaction.replace(R.id.RelativeLayoutForConnectedOptions, currentShowingFragment);
                 fragmentTransaction.show(currentShowingFragment);
                 fragmentTransaction.commit();
             });
         }
+        return true;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_connected, container, false);
-
-        DTManager.getInstance().setDtManagerConnCallBack(new DTManagerConnCallBackHandler());
-
-        textViewForShowingConnectionStatus = v.findViewById(R.id.TextViewForShowingConnectionStatus);
+    private void updateConnectionStatus() {
         DTConnectedClient connectedClient = DTManager.getInstance().getConnectedClient();
-
-        if(connectedClient != null) {
-            String status = SpreadUpApplication.getInstance().getString(R.string.connection_status_yes) + connectedClient.getName();
-            textViewForShowingConnectionStatus.setText(status);
-            textViewForShowingConnectionStatus.setBackgroundResource(R.color.connected_color);
-        } else {
-            String status = SpreadUpApplication.getInstance().getString(R.string.connection_status_no);
-            textViewForShowingConnectionStatus.setText(status);
-            textViewForShowingConnectionStatus.setBackgroundResource(R.color.disconnected_color);
+        if(getActivity() != null) {
+            getActivity().runOnUiThread(() -> {
+                if(connectedClient == null) { // disconnected
+                    String status = SpreadUpApplication.getInstance().getString(R.string.connection_status_no);
+                    textViewForShowingConnectionStatus.setText(status);
+                    textViewForShowingConnectionStatus.setBackgroundResource(R.color.disconnected_color);
+                } else { // connected
+                    String status = SpreadUpApplication.getInstance().getString(R.string.connection_status_yes) + connectedClient.getName();
+                    textViewForShowingConnectionStatus.setText(status);
+                    textViewForShowingConnectionStatus.setBackgroundResource(R.color.connected_color);
+                }
+            });
         }
-
-        return v;
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        myFragmentCallBacks.setBackButtonEnabled(true);
-        myFragmentCallBacks.setActivityTitle(SpreadUpApplication.getInstance().getString(R.string.fragment_title_connected));
     }
 
     private class DTManagerConnCallBackHandler implements DTManager.DTManagerConnCallBack {
 
         @Override
         public void onDisconnectDevice() {
-            if(getActivity() != null) {
-                getActivity().runOnUiThread(() -> {
-                    String status = SpreadUpApplication.getInstance().getString(R.string.connection_status_no);
-                    textViewForShowingConnectionStatus.setText(status);
-                    textViewForShowingConnectionStatus.setBackgroundResource(R.color.disconnected_color);
-                });
-            }
+            updateConnectionStatus();
         }
 
         @Override
         public void onConnectDevice() {
-            if(getActivity() != null) {
-                getActivity().runOnUiThread(() -> {
-                    DTConnectedClient connectedClient = DTManager.getInstance().getConnectedClient();
-                    String status = SpreadUpApplication.getInstance().getString(R.string.connection_status_yes) + connectedClient.getName();
-                    textViewForShowingConnectionStatus.setText(status);
-                    textViewForShowingConnectionStatus.setBackgroundResource(R.color.connected_color);
-                });
-            }
+            updateConnectionStatus();
         }
 
         @Override
         public void onConnectionFailed() {
-            if(getActivity() != null) {
-                getActivity().runOnUiThread(() -> {
-                    String status = SpreadUpApplication.getInstance().getString(R.string.connection_status_failed);
-                    textViewForShowingConnectionStatus.setText(status);
-                    textViewForShowingConnectionStatus.setBackgroundResource(R.color.disconnected_color);
-                });
+            updateConnectionStatus();
+        }
+    }
+
+    private class MyNavigationItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_send_file:
+                    return showSendDataFragment();
+                case R.id.navigation_receiver:
+                    return showReceivingStatusFragment();
+                case R.id.navigation_sender:
+                    return showSendingStatusFragment();
             }
+            return false;
         }
     }
 }
